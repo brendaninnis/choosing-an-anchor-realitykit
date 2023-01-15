8509b618466a38c8d9ebd8dc4c23af05d77eb367
Brendan Innis <innisbrendan@gmail.com>
2023-01-15 10:59:39 -0800

    Initial Commit

---
+++ Choosing an Anchor for Your RealityKit Scene/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Choosing an Anchor for Your RealityKit Scene
+//
+//  Created by Brendan Innis on 2023-01-15.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
